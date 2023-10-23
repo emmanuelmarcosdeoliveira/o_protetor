30b47d793f6944cdffa07cd9a01fd957cfce839d
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-10-23 17:35:03 -0300

    :heavy_plus_sign:  gulp-sass and watch dependencies configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-function testeGulp(cb) {
-  console.log("Olá mundo Gulp");
-  cb();
-}  
+const gulp =require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-exports.default = testeGulp;
+
+function compilaSass(){
+  return gulp.src('./src/styles/sass/**/*.scss') // indicando aonde esta os arquivos
+.pipe(sass({outputStyle: 'compressed'})) // chammando o sass para a compilação e informando que os arquivos seram compressos
+.pipe(gulp.dest('./dist/css')); // indicando onde os arquivos compressos deveram ser criados
+}
+
+exports.default = compilaSass;
+exports.watch = function() {
+	gulp.watch('./src/styles/sass/**/*.scss', gulp.parallel(compilaSass))
+}
+// Aqui criamos uma função em paralelo para a execução do watch 
+
