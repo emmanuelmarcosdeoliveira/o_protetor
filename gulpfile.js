33d4bb4771240eead792874b11a77ceadf975b82
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-10-25 22:39:55 -0300

    :construction: section hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
-const gulp =require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const gulpImagemin = require("gulp-imagemin");
 
-
-function compilaSass(){
-  return gulp.src('./src/styles/sass/**/*.scss') // indicando aonde esta os arquivos
-.pipe(sass({outputStyle: 'compressed'})) // chammando o sass para a compilação e informando que os arquivos seram compressos
-.pipe(gulp.dest('./dist/css')); // indicando onde os arquivos compressos deveram ser criados
+function compilaSass() {
+  return gulp
+    .src("./src/styles/sass/**/*.scss") // indicando aonde esta os arquivos
+    .pipe(sass({ outputStyle: "compressed" })) // chammando o sass para a compilação e informando que os arquivos seram compressos
+    .pipe(gulp.dest("./dist/css")); // indicando onde os arquivos compressos deveram ser criados
 }
 
-exports.default = compilaSass;
-exports.watch = function() {
-	gulp.watch('./src/styles/sass/**/*.scss', gulp.parallel(compilaSass))
+function comprimeImagens() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(gulpImagemin())
+    .pipe(gulp.dest("./dist/images/"));
 }
-// Aqui criamos uma função em paralelo para a execução do watch 
 
+exports.default = gulp.parallel(compilaSass, comprimeImagens);
+exports.watch = function () {
+  gulp.watch("./src/styles/sass/**/*.scss", gulp.parallel(compilaSass));
+};
+// Aqui criamos uma função em paralelo para a execução do watch
