0c817d84703a15b7ee830b1ccaba1cf9d09185d5
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-11-02 17:46:39 -0300

    ::heavy_plus_sign: Dependência uglify adicionada

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const gulpImagemin = require("gulp-imagemin");
+const uglify = require('gulp-uglify');
+
+function scripts() {
+  return gulp.src('./src/scripts/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/scripts'))
+}
+
 
 function compilaSass() {
   return gulp
@@ -16,8 +24,9 @@ function comprimeImagens() {
     .pipe(gulp.dest("./dist/images/"));
 }
 
-exports.default = gulp.parallel(compilaSass, comprimeImagens);
+exports.default = gulp.parallel(compilaSass, comprimeImagens, scripts);
 exports.watch = function () {
   gulp.watch("./src/styles/sass/**/*.scss", gulp.parallel(compilaSass));
+  gulp.watch('./src/scritps/*.js', gulp.parallel(scripts))
 };
 // Aqui criamos uma função em paralelo para a execução do watch
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
   const question = document.querySelectorAll("[data-faq-question]");
+  const heroSection = document.querySelector('.hero');
+  const alturaHero = heroSection.clientHeight;
 
+  window.addEventListener('scroll', function(){
+   const posicaoAtual = window.scrollY;
+    if(posicaoAtual < alturaHero) {
+     ocultaBotaoHeader();
+    } else {
+     exibeBotaoHeader(); 
+    }
+ 
+ 
+  })
+
+
+  //Seção de Atrações programação das Abas
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (botao) {
       const abaAlvo = botao.target.dataset.tabButton;
@@ -12,11 +27,22 @@ document.addEventListener("DOMContentLoaded", function () {
       botao.target.classList.add("is--active");
     });
   }
-
+  // Seção FAQ Acordion
   for (let i = 0; i < question.length; i++) {
     question[i].addEventListener("click", abreFechaResposta);
   }
 });
+
+function ocultaBotaoHeader(){
+  const header = document.querySelector('header');
+ header.classList.add('header--is-hidden')
+}
+
+function exibeBotaoHeader(){
+  const header = document.querySelector('header');
+ header.classList.remove('header--is-hidden')
+}
+
 
 function abreFechaResposta(elemento) {
   const classe = "faq__question__item--is-open";
